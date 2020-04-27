b13cc6cb3988a31a31024e2a1836863faff4f0ea
qjutard <quentin.jutard@locean-ipsl.upmc.fr>
2020-04-27 17:58:07 +0200

    implemented sankey diagrams to plot improvements and degradations for a float with adjusted parameters

--- main.R
+++ main.R
@@ -5,6 +5,11 @@ library(RColorBrewer)
 library(gridExtra)
 library(parallel)
 library(jsonlite)
+library(networkD3)
+library(dplyr)
+library(tibble)
+library(tidyr)
+library(htmlwidgets)
 
 source("~/Documents/radiometry/RT_QC_radiometry_function_oao_2.R")
 
@@ -38,7 +43,7 @@ for (i in 1:length(files_corr)) {
 files_corr = paste(path_to_netcdf, files_corr, sep="")
 
 
-plot_QC <- function(filename, with_corr=FALSE) {
+plot_QC <- function(filename, with_corr=FALSE, do_plot=TRUE) {
  
     filenc = nc_open(filename)
     
@@ -69,7 +74,11 @@ plot_QC <- function(filename, with_corr=FALSE) {
     	IRR_412 = ncvar_get(filenc, IRR412_NAME[i])[,id_prof]
     	IRR_490 = ncvar_get(filenc, IRR490_NAME[i])[,id_prof]
     	PAR = ncvar_get(filenc, PAR_NAME[i])[,id_prof]
-    
+
+		if (length(which(!is.na(IRR_380))) < 5) {
+			return(list("type380"="0", "type412"="0", "type490"="0", "typePAR"="0"))
+		}   
+ 
     	QC_flags = RT_QC_radiometry(PRES, IRR_380, IRR_412, IRR_490, PAR)
     
     	dataf = data.frame(QC_flags)
@@ -88,7 +97,8 @@ plot_QC <- function(filename, with_corr=FALSE) {
     	dataf$FLAG_412 = factor(dataf$FLAG_412 , levels=c("1", "2", "3", "4", NA))
     	dataf$FLAG_490 = factor(dataf$FLAG_490 , levels=c("1", "2", "3", "4", NA))
     	dataf$FLAG_PAR = factor(dataf$FLAG_PAR , levels=c("1", "2", "3", "4", NA))
-    
+   		
+		if (do_plot) { 
     	col_1 = "#869C66"
     	col_2 = "#FFCA0B"
     	col_3 = "#FF9502"
@@ -177,24 +187,28 @@ plot_QC <- function(filename, with_corr=FALSE) {
 		} else {
 			gg5_adjusted = grid.arrange(g1, g2, g3, g4, nrow=1)
 		}
+		}
 		
 	}
     
 
     if (str_sub(filename,-4,-4) == "D") {
-        plot_name = paste("radiomerty_QC_", str_sub(filename, -15, -4), ".png", sep="")
+        plot_name = paste("radiometry_QC_", str_sub(filename, -15, -4), ".png", sep="")
     } else {
-        plot_name = paste("radiomerty_QC_", str_sub(filename, -14, -4), ".png", sep="")
+        plot_name = paste("radiometry_QC_", str_sub(filename, -14, -4), ".png", sep="")
     }
 
-	if (with_corr){
-    	png(plot_name, width = 800, height = 800)
-    	grid.arrange(gg5, gg5_adjusted, nrow=2)
-	} else {
-    	png(plot_name, width = 800, height = 400)
-		grid.arrange(gg5)
+	if (do_plot) {
+		if (with_corr){
+    		png(plot_name, width = 800, height = 800)
+    		grid.arrange(gg5, gg5_adjusted, nrow=2)
+    		dev.off()
+		} else {
+    		png(plot_name, width = 800, height = 400)
+			grid.arrange(gg5)
+    		dev.off()
+		}
 	}
-    dev.off()
     
     nc_close(filenc)
     
@@ -204,6 +218,53 @@ plot_QC <- function(filename, with_corr=FALSE) {
 
 
 num_cores = detectCores()
-M = mcmapply(plot_QC, files_corr, mc.cores=num_cores, SIMPLIFY=FALSE, MoreArgs=list(with_corr=TRUE))
+M = mcmapply(plot_QC, files_corr, mc.cores=num_cores, SIMPLIFY=FALSE, MoreArgs=list(with_corr=FALSE, do_plot=FALSE))
+
+Mcorr = mcmapply(plot_QC, files_corr, mc.cores=num_cores, SIMPLIFY=FALSE, MoreArgs=list(with_corr=TRUE, do_plot=FALSE))
+
+A = t(array(unlist(M, use.names=FALSE), dim=c(4,length(M))))
+Acorr = t(array(unlist(Mcorr, use.names=FALSE), dim=c(4,length(M))))
+
+param_name = c("IRR380", "IRR412", "IRR490", "PAR")
+sankey_names = paste0("sankey_", WMO, "_", param_name, ".html")
+
+for (n in 1:4) {
+	type = A[,n]
+	type_corr = Acorr[,n]
+
+	change = matrix(0, 4, 4)
+	rownames(change) = c("1", "2", "3", "4")
+	colnames(change) = c("corr_1", "corr_2", "corr_3", "corr_4")
+
+	for (i in 1:4) {
+		for (j in 1:4) {
+			change[i,j] = length(which(type==as.character(i) & type_corr==as.character(j)))
+		}
+	} 
+
+	links = change %>%
+		as.data.frame() %>%
+		rownames_to_column(var="source") %>%
+		gather(key="target", value="value", -1) %>%
+		filter(value != 0)
+
+	nodes = data.frame(
+		name = c(as.character(links$source), as.character(links$target)) %>%
+		unique()
+		)
+
+	links$IDsource = match(links$source, nodes$name) -1
+	links$IDtarget = match(links$target, nodes$name) -1
+
+	my_color = 'd3.scaleOrdinal() .domain(["1", "2", "3", "4", "corr_1", "corr_2", "corr_3", "corr_4"]) .range(["#869C66", "#FFCA0B", "#FF9502", "#D43501", "#869C66", "#FFCA0B", "#FF9502", "#D43501"])'
+
+	p1 = sankeyNetwork(Links = links, Nodes = nodes,
+					Source = "IDsource", Target = "IDtarget",
+					Value = "value", NodeID = "name",
+					colourScale = my_color)
+
+	saveWidget(p1, file=sankey_names[n], selfcontained=FALSE)
+}
+
 
 #write_json(M, path="profile_types.json", auto_unbox=T, pretty=T, null="list")
