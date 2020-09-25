1fcb965ea3b94ba9a0f8d5860013df066dd6a7c6
qjutard <quentin.jutard@locean-ipsl.upmc.fr>
2020-09-25 17:27:41 +0200

    comments

--- sensor_temp.R
+++ sensor_temp.R
@@ -15,8 +15,8 @@ sensor_temp <- function(TEMP, PRES_TEMP, PRES_PARAM, material="PEEK") {
 	asc_speed = 0.1 # m/s
 
 	Tw = rev(TEMP[!is.na(TEMP) & !is.na(PRES_TEMP)])
-	Tw_pres = rev(PRES_TEMP[!is.na(TEMP) & !is.na(PRES_TEMP)])
-	Tw_time = (max(Tw_pres) - Tw_pres) / asc_speed #assuming 1m/db for now
+	Tw_pres = rev(PRES_TEMP[!is.na(TEMP) & !is.na(PRES_TEMP)]) #pressure associated to Tw measurements
+	Tw_time = (max(Tw_pres) - Tw_pres) / asc_speed #assuming 1m/db for now, time associated to Tw measurements
 
 	n_time = length(Tw)
 	
@@ -26,8 +26,8 @@ sensor_temp <- function(TEMP, PRES_TEMP, PRES_PARAM, material="PEEK") {
 	
 	Ts = rep(NA, n_time)
 	Ts[1] = Tw[1]
-	Ts_time = Tw_time + delta_t
-	Ts_pres = max(Tw_pres) - Ts_time*asc_speed
+	Ts_time = Tw_time + delta_t # time associated to Ts estimates
+	Ts_pres = max(Tw_pres) - Ts_time*asc_speed # pressure associated to Ts estimates
 
 	for (n in 2:n_time) {
 		Ts[n] = Ts[n-1] + k * (Tw_time[n]-Tw_time[n-1]) * (Tw[n-1]-Ts[n-1])
