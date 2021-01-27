105cae8a5b9bd5efa2d1a03c763ae5f1f63991a4
qjutard <quentin.jutard@locean-ipsl.upmc.fr>
2021-01-27 13:26:03 +0100

    restricted to floats with good data

--- single_scripts/all_profile_map.R
+++ single_scripts/all_profile_map.R
@@ -13,6 +13,12 @@ source("~/Documents/radiometry/single_scripts/survey.R")
 
 #corr_list = corr_list[which(corr_list$material!="LIVE"),]
 corr_list = corr_list[which(corr_list$material!="LIVE" & corr_list$num_night>0 & corr_list$num_drift>=0.8*corr_list$num_prof),]
+corrected_with_alternative = c("6901580", "6901866", "6901650", "6902968", "6901647", "6902547") # these floats have good QC data but were corrected with B and/or day methods
+corr_list$corrected_with_alternative = rep(FALSE, length(corr_list$WMO))
+for (the_WMO in corrected_with_alternative) {
+	corr_list$corrected_with_alternative[which(corr_list$WMO == the_WMO)] = TRUE
+}
+
 
 pattern = paste0(corr_list$WMO)
 #pattern[126] = paste0(corr_list$WMO[126], "_[0-9][05]2.nc") #This one does not have profile 001 ?
@@ -29,6 +35,7 @@ match_WMO = str_sub(index_ifremer$file[matches], 10, 16)
 status = rep(NA, length(lat))
 status[which(corr_list$corrected[match(match_WMO, corr_list$WMO)]==1)] = "Corrected"
 status[which(corr_list$corrected[match(match_WMO, corr_list$WMO)]==0)] = "Not corrected"
+status[which(corr_list$corrected_with_alternative[match(match_WMO, corr_list$WMO)])] = "Corrected with alternative method(s)"
 #status[which(is.na(corr_list$corrected))] = "TODO"
 
 lat_range = c(30, 47)
@@ -52,7 +59,7 @@ map_world = world[which(world$long >= -180 & world$long <= 180),]
 
 #my_palette = c(wes_palette("Rushmore1")[3], wes_palette("Rushmore1")[5])
 #my_palette = c(wes_palette("Cavalcanti1")[2], wes_palette("Cavalcanti1")[5])
-my_palette = brewer.pal(n=3, name="Dark2")[1:2]
+my_palette = brewer.pal(n=6, name="Dark2")[c(1,2,6)]
 
 
 g1 = ggplot() +
@@ -80,6 +87,7 @@ g3 = ggplot() +
 g3.5 = ggplot() +
 	geom_polygon(data=map_world, aes(x=long, y=lat, group=group), fill="#dddddd")+
 	geom_point(aes(x=lon_plot[status_plot=="Corrected"], y=lat_plot[status_plot=="Corrected"]), color=my_palette[1], size=0.7) +
+	geom_point(aes(x=lon_plot[status_plot=="Corrected with alternative method(s)"], y=lat_plot[status_plot=="Corrected with alternative method(s)"]), color=my_palette[3], size=0.7) +
 	geom_point(aes(x=lon_plot[status_plot=="Not corrected"], y=lat_plot[status_plot=="Not corrected"]), color=my_palette[2], size=0.7) +
 	annotate("rect", xmin=lon_range[1], xmax=lon_range[2], ymin=lat_range[1], ymax = lat_range[2], color="black", fill="black", alpha=0.5) +
 	scale_x_continuous(breaks=seq(-180,180,30)) +
@@ -88,6 +96,7 @@ g3.5 = ggplot() +
 	labs(color = "Correction status", x="Longitude", y="Latitude") 
 g4 = ggplot() +
 	geom_polygon(data=map_world, aes(x=long, y=lat, group=group), fill="#dddddd")+
+	geom_point(aes(x=lon_plot[status_plot=="Corrected with alternative method(s)"], y=lat_plot[status_plot=="Corrected with alternative method(s)"]), color=my_palette[3], size=0.7) +
 	geom_point(aes(x=lon_plot[status_plot=="Not corrected"], y=lat_plot[status_plot=="Not corrected"]), color=my_palette[2], size=0.7) +
 	annotate("rect", xmin=lon_range[1], xmax=lon_range[2], ymin=lat_range[1], ymax = lat_range[2], color="black", fill="black", alpha=0.5) +
 	scale_x_continuous(breaks=seq(-180,180,30)) +
@@ -102,11 +111,13 @@ g5 = ggplot() +
 g5.5 = ggplot() +
 	geom_polygon(data=map_mb, aes(x=long, y=lat, group=group), fill="#dddddd")+
 	geom_point(aes(x=lon_plot_mb[status_plot_mb=="Corrected"], y=lat_plot_mb[status_plot_mb=="Corrected"]), color=my_palette[1], size=0.7) +
+	geom_point(aes(x=lon_plot_mb[status_plot_mb=="Corrected with alternative method(s)"], y=lat_plot_mb[status_plot_mb=="Corrected with alternative method(s)"]), color=my_palette[3], size=0.7) +
 	geom_point(aes(x=lon_plot_mb[status_plot_mb=="Not corrected"], y=lat_plot_mb[status_plot_mb=="Not corrected"]), color=my_palette[2], size=0.7) +
 	theme_bw() +
 	theme(legend.position="none", axis.title.x=element_blank(), axis.title.y=element_blank())
 g6 = ggplot() +
 	geom_polygon(data=map_mb, aes(x=long, y=lat, group=group), fill="#dddddd")+
+	geom_point(aes(x=lon_plot_mb[status_plot_mb=="Corrected with alternative method(s)"], y=lat_plot_mb[status_plot_mb=="Corrected with alternative method(s)"]), color=my_palette[3], size=0.7) +
 	geom_point(aes(x=lon_plot_mb[status_plot_mb=="Not corrected"], y=lat_plot_mb[status_plot_mb=="Not corrected"]), color=my_palette[2], size=0.7) +
 	theme_bw() +
 	theme(legend.position="none", axis.title.x=element_blank(), axis.title.y=element_blank())
