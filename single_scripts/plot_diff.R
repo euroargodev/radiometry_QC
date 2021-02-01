0d8b14789a92a88bd9c1464196dadccb05e76e7b
qjutard <quentin.jutard@locean-ipsl.upmc.fr>
2021-02-01 15:08:05 +0100

    possibility to limit y axis

--- single_scripts/plot_diff.R
+++ single_scripts/plot_diff.R
@@ -7,24 +7,24 @@ library(latex2exp)
 source("~/Documents/radiometry/get_matches.R")
 source("~/Documents/radiometry/sensor_temp.R")
 
-WMO = "6901474"
-profile = "006"
-#param = "DOWN_IRRADIANCE380"
-#param = "DOWNWELLING_PAR"
+WMO = "6902828"
+profile = "341"
+pressure_limits = NULL # c(100, 0)
+#comment = "Mediterranean sea,\n 2017-09-09"
 PARAM_NAMES = c("DOWNWELLING_PAR", "DOWN_IRRADIANCE380", "DOWN_IRRADIANCE412", "DOWN_IRRADIANCE490")
 irr_labels = c("PAR", "E_d(380)", "E_d(412)", "E_d(490)")
 names(irr_labels) = PARAM_NAMES
 units = c("$\\mu mol\\cdot m^{-2}\\cdot s^{-1}$", "$W\\cdot m^{-2}\\cdot nm^{-1}$", "$W\\cdot m^{-2}\\cdot nm^{-1}$", "$W\\cdot m^{-2}\\cdot nm^{-1}$")
 names(units) = PARAM_NAMES
 limits = list()
-limits[[PARAM_NAMES[1]]] = c(-1e-1, 3e-1)
-limits[[PARAM_NAMES[2]]] = c(-1e-4, 3e-4)
-limits[[PARAM_NAMES[3]]] = c(-1e-4, 3e-4)
-limits[[PARAM_NAMES[4]]] = c(-1e-4, 3e-4)
+limits[[PARAM_NAMES[1]]] = c(-0.15, 0.15)
+limits[[PARAM_NAMES[2]]] = c(-5e-5, 1e-4)
+limits[[PARAM_NAMES[3]]] = c(-5e-5, 2e-4)
+limits[[PARAM_NAMES[4]]] = c(-5e-5, 1e-4)
 
 
 file_A = paste0("/mnt/c/DATA/ftp.ifremer.fr/ifremer/argo/dac/coriolis/", WMO, "/profiles/BD", WMO, "_", profile, ".nc")
-file_D = paste0("~/Documents/radiometry_QC/", WMO, "/RADM_profiles/BD", WMO, "_", profile, ".nc")
+#file_D = paste0("~/Documents/radiometry_QC/", WMO, "/RADM_profiles/BD", WMO, "_", profile, ".nc")
 file_core = paste0("/mnt/c/DATA/ftp.ifremer.fr/ifremer/argo/dac/coriolis/", WMO, "/profiles/D", WMO, "_", profile, ".nc")
 
 path_to_netcdf=""
@@ -54,20 +54,26 @@ get_param <- function(filename, PARAM_NAME, adj = T) {
 for (param in PARAM_NAMES) { 
 
 match_A = get_Ts_match(path_to_netcdf, file_A, param)
-Irr_D = get_param(file_D, param)
+Irr_D = get_param(file_A, param)
 
 nna = which(!is.na(match_A$PARAM) & !is.na(match_A$Ts) & !is.na(match_A$PRES))
 
 df = data.frame("PARAM_A"=match_A$PARAM[nna], "PARAM_D"=Irr_D[nna], "PRES"=match_A$PRES[nna], "Ts"=match_A$Ts[nna])
-df$ERROR = pmax(2.5e-5, df$PARAM_D*0.02)
+if (param == "DOWNWELLING_PAR") {
+	df$ERROR = pmax(3e-2, df$PARAM_D*0.05)
+} else {
+	df$ERROR = pmax(2.5e-5, df$PARAM_D*0.02)
+}
 
 ab = lm(df$PARAM_D-df$PARAM_A ~ df$Ts)
 
 g1 = ggplot(df, aes(x=Ts, y=PRES)) +
-	scale_y_reverse(name="Pressure (dbar)") +
+	scale_y_reverse(name="Pressure (dbar)", limits=pressure_limits) +
 	scale_x_continuous(
 		name = "Sensor Temperature (°C)",
 		sec.axis = sec_axis(trans=~.*ab$coefficients[2] + ab$coefficients[1], name=TeX(paste0("Applied offset (", units[param], ")")), 
+		labels = function(x) format(x, scientific=TRUE),
+		guide = guide_axis(check.overlap=TRUE)
 		#breaks = waiver()
 		#breaks = c(-4e-5, -1.5e-5, 1e-5)
 		)
@@ -78,11 +84,13 @@ g1 = ggplot(df, aes(x=Ts, y=PRES)) +
 	theme(plot.margin=margin(t=5.5, b=5.5, l=5.5, r=15, unit="pt"))
 
 g2 = ggplot(df, aes(x=PARAM_A, y=PRES)) +
-	scale_y_reverse(name="Pressure (dbar)") +
+	scale_y_reverse(name="Pressure (dbar)", limits=pressure_limits) +
 	scale_x_continuous(
 		limits=limits[[param]],
 		name = TeX(paste0(irr_labels[param]," (", units[param], ")")),
-		sec.axis = sec_axis(trans=~.*1, name=TeX(paste0("Applied offset (", units[param], ")"))) 
+		sec.axis = sec_axis(trans=~.*1, name=TeX(paste0("Applied offset (", units[param], ")"))), 
+		labels = function(x) format(x, scientific=TRUE),
+		guide = guide_axis(check.overlap=TRUE)
 ) +
 	geom_vline(xintercept=0, linetype="dashed") +
 	scale_color_manual(values = c("Not corrected"="black", "Corrected"="red")) +
@@ -95,19 +103,22 @@ g2 = ggplot(df, aes(x=PARAM_A, y=PRES)) +
 		axis.text.x.top = element_text(color = "white"),
 		axis.ticks.x.top = element_line(size=0),
 		legend.title = element_blank(),
-		legend.position = c(0.01, 0.99),
-		legend.justification = c("left", "top"),
+		#legend.position = c(0.01, 0.99),
+		#legend.justification = c("left", "top"),
+		legend.position = c(0.99, 0.01),
+		legend.justification = c("right", "bottom"),
 		legend.background = element_rect(fill="grey90")
 	)
 
 min_ribbon = min(df$PARAM_A[which(df$PARAM_A>0)], df$PARAM_D[which(df$PARAM_D>0)])
 
 g3 = ggplot(df, aes(x=PARAM_A, y=PRES)) +
-	scale_y_reverse(name="Pressure (dbar)") +
+	scale_y_reverse(name="Pressure (dbar)", limits=pressure_limits) +
 	scale_x_log10(
 		expand=expansion(mult=c(0,0.04)),
 		name = TeX(paste0(irr_labels[param]," (", units[param], ")")),
-		sec.axis = sec_axis(trans=~.*1, name=TeX(paste0("Applied offset (", units[param], ")"))) 
+		sec.axis = sec_axis(trans=~.*1, name=TeX(paste0("Applied offset (", units[param], ")"))),
+		labels = function(x) format(x, scientific=TRUE)
 ) + 
 	#geom_vline(xintercept=0, linetype="dashed") +
     geom_ribbon(aes(xmin=pmax(min_ribbon,PARAM_D-ERROR), xmax=PARAM_D+ERROR), fill="grey60", alpha=0.5) +
@@ -120,15 +131,17 @@ g3 = ggplot(df, aes(x=PARAM_A, y=PRES)) +
 		axis.text.x.top = element_text(color = "white"),
 		axis.ticks.x.top = element_line(size=0),
 		legend.title = element_blank(),
-		legend.position = c(0.01, 0.99),
-		legend.justification = c("left", "top"),
+		#legend.position = c(0.01, 0.99),
+		#legend.justification = c("left", "top"),
+		legend.position = c(0.99, 0.01),
+		legend.justification = c("right", "bottom"),
 		legend.background = element_rect(fill="grey90")
 	) 
 
 #gt = grid.arrange(g3, g2, g1, nrow=1)
 #gt
 
-png(filename=paste0("~/Documents/radiometry_QC/plot_diff/plot_correction_", WMO, "_", profile, "_", param, ".png"), width=800, height=300)
+png(filename=paste0("~/Documents/radiometry_QC/plot_diff/collection_2/plot_correction_", WMO, "_", profile, "_", param, ".png"), width=800, height=300)
 grid.arrange(g3, g2, g1, nrow=1)
 dev.off()
 
